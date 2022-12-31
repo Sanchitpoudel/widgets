7b60ca28af3a6f31ab05f12ffd978bd7b822d2bb
Sanchit poudel <50201318+Sanchitpoudel@users.noreply.github.com>
2022-12-31 14:29:34 +0545

    commit

--- lib/mediaquery.dart
+++ lib/mediaquery.dart
@@ -1,9 +1,31 @@
+// ignore_for_file: prefer_interpolation_to_compose_strings
+
 import 'package:flutter/material.dart';
 
 class MediaQueryPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          
+          children: [
+            Text(
+              'Height: ' + MediaQuery.of(context).size.height.toString(),
+            ),
+            Text(
+              'Width: ' + MediaQuery.of(context).size.width.toString(),
+            ),
+            Text(
+              'Aspect Ratio: ' + MediaQuery.of(context).size.aspectRatio.toString(),
+            ),Text(
+              'Orientation: ' + MediaQuery.of(context).orientation.toString(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
